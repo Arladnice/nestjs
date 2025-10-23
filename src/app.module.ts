4d8e5f9d8f83fd67fe6bedfa1cbfa0e41b177e6f
Denis Arlakov <denarlakov12@gmail.com>
2025-10-23 16:52:36 +0300

    2:01:14

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MovieModule } from './movie/movie.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'root',
+      password: '123456',
+      database: 'nestjs-course',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    MovieModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
-  imports: [MovieModule],
 })
 export class AppModule {}
--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Headers,
+  Post,
+  Query,
+  Req,
+} from '@nestjs/common';
 
 @Controller('movies')
 export class MovieController {
@@ -6,4 +14,21 @@ export class MovieController {
   findAll(@Query() query: any) {
     return JSON.stringify(query);
   }
+
+  @Post()
+  create(@Body() body: { title: string; genre: string }) {
+    return body;
+  }
+
+  @Get('headers')
+  getHeaders(@Headers() headers: any) {
+    return headers;
+  }
+
+  @Get('user-agent')
+  getUserAgent(@Headers('user-agent') userAgent: string) {
+    return userAgent;
+  }
+
+  
 }
