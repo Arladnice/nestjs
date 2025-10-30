c9328f1c6d8b80ee278d2c933b9c359c118937d5
Denis Arlakov <denarlakov12@gmail.com>
2025-10-30 15:33:07 +0300

    4:46:00

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MovieModule } from './movie/movie.module';
 import { ConfigModule } from '@nestjs/config';
-import { ReviewModule } from './review/review.module';
-import { ActorModule } from './actor/actor.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
@@ -13,9 +10,6 @@ import { PrismaModule } from './prisma/prisma.module';
       isGlobal: true,
     }),
     PrismaModule,
-    MovieModule,
-    ReviewModule,
-    ActorModule,
   ],
   controllers: [AppController],
   providers: [AppService],
