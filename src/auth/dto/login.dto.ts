bcddcd4390e46a42c43418b86e22746ac7808346
Denis Arlakov <denarlakov12@gmail.com>
2025-11-06 10:07:07 +0300

    6:17:58

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -7,17 +7,7 @@ import {
   MinLength,
 } from 'class-validator';
 
-export class RegisterRequest {
-  @ApiProperty({
-    description: 'Отображаемое имя',
-    example: 'John Doe',
-    maxLength: 50,
-  })
-  @IsString({ message: 'Имя должно быть строкой' })
-  @IsNotEmpty({ message: 'Имя обязательно для заполнения' })
-  @MaxLength(50, { message: 'Имя не должно превышать 50 символов' })
-  name: string;
-
+export class LoginRequest {
   @ApiProperty({
     description: 'Почтовый адрес',
     example: 'darlakov@mail.ru',
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { DocumentBuilder } from '@nestjs/swagger';
-import { SwaggerModule } from '@nestjs/swagger';
+import cookieParser from 'cookie-parser';
+import { setupSwagger } from './utils/swagger.util';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(cookieParser());
+
   app.useGlobalPipes(new ValidationPipe());
 
-  const config = new DocumentBuilder()
-    .setTitle('NestJS Course')
-    .setDescription('NestJS Course API')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config, {
-    operationIdFactory: (controllerKey, methodKey) =>
-      `${controllerKey}-${methodKey}`,
-  });
-  SwaggerModule.setup('api', app, document, {
-    jsonDocumentUrl: '/swager.json',
-  });
+  setupSwagger(app);
 
   await app.listen(3000);
 }
