ee09c9495ae7181348ea3f3701b6d84995e12cc3
Denis Arlakov <denarlakov12@gmail.com>
2025-11-06 14:10:15 +0300

    6:35:55

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import {
   Body,
   Controller,
+  Get,
   HttpCode,
   HttpStatus,
   Post,
@@ -11,11 +12,32 @@ import { AuthService } from './auth.service';
 import { RegisterRequest } from './dto/register.dto';
 import { LoginRequest } from './dto/login.dto';
 import type { Request, Response } from 'express';
+import {
+  ApiBadRequestResponse,
+  ApiConflictResponse,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { AuthResponse } from './dto/auth.dto';
+import { Authorization } from './decorators/authorization.decorator';
+import { Authorized } from './decorators/authorized.decorator';
+import type { User } from '@prisma/client';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @ApiOperation({
+    summary: 'Создание пользователя',
+    description: 'Создание нового пользователя',
+  })
+  @ApiOkResponse({ type: AuthResponse })
+  @ApiBadRequestResponse({ description: 'Некорректный запрос' })
+  @ApiConflictResponse({
+    description: 'Пользователь с такой почтой уже существует',
+  })
   @Post('register')
   @HttpCode(HttpStatus.CREATED)
   async register(
@@ -25,6 +47,13 @@ export class AuthController {
     return await this.authService.register(res, dto);
   }
 
+  @ApiOperation({
+    summary: 'Вход в систему',
+    description: 'Вход в систему пользователя',
+  })
+  @ApiOkResponse({ type: AuthResponse })
+  @ApiBadRequestResponse({ description: 'Некорректный запрос' })
+  @ApiNotFoundResponse({ description: 'Пользователь не найден' })
   @Post('login')
   @HttpCode(HttpStatus.OK)
   async login(
@@ -34,6 +63,12 @@ export class AuthController {
     return await this.authService.login(res, dto);
   }
 
+  @ApiOperation({
+    summary: 'Обновление токена',
+    description: 'Обновление токена доступа',
+  })
+  @ApiOkResponse({ type: AuthResponse })
+  @ApiUnauthorizedResponse({ description: 'Недействительный refresh-токен' })
   @Post('refresh')
   @HttpCode(HttpStatus.OK)
   async refresh(
@@ -43,9 +78,19 @@ export class AuthController {
     return await this.authService.refresh(req, res);
   }
 
+  @ApiOperation({
+    summary: 'Выход из системы',
+  })
   @Post('logout')
   @HttpCode(HttpStatus.OK)
   logout(@Res({ passthrough: true }) res: Response) {
     return this.authService.logout(res);
+  }
+
+  @Authorization()
+  @Get('@me')
+  @HttpCode(HttpStatus.OK)
+  me(@Authorized() user: User) {
+    return user;
   }
 }
